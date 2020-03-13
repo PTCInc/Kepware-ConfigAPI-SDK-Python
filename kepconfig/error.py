238142b12c1c3f8e8456a46ead70915f65c07b38
Raymond Labbe <34889968+redraymon@users.noreply.github.com>
2020-03-13 13:21:09 -0400

    Revert "Update Copyright"
    This reverts commit 66d358917beaff9b1e673db0d90448ad2621e801.

--- kepconfig/error.py
+++ kepconfig/error.py
@@ -1,10 +1,4 @@
-# -------------------------------------------------------------------------
-# Copyright (c) 2020, PTC Inc. and/or all its affiliates. All rights reserved.
-# See License.txt in the project root for
-# license information.
-# --------------------------------------------------------------------------
-
-""":mod:`error` defines exception classes raised by Kepconfig
+"""Exception classes raised by Kepconfig
 
 KepURLError - Inherits responses from the urllib URLError exceptions.
 
@@ -18,9 +12,6 @@ handle an exception like a regular response.
 __all__ = ['KepURLError', 'KepHTTPError']
 
 class KepURLError(Exception):
-    """Exception class raised by Kepconfig that inherits responses from 
-    the urllib URLError exceptions.
-    """
 
     def __init__(self, reason, url):
         self.reason = reason
@@ -30,13 +21,7 @@ class KepURLError(Exception):
         return '<urlopen error %s>' % self.reason
 
 class KepHTTPError(Exception):
-    """Exception class raised by Kepconfig that inherits responses from 
-    the urllib HTTPError exceptions. This exception class is also a valid 
-    HTTP response instance.  It behaves this way because HTTP protocol 
-    errors are validresponses, with a status code, headers, and a body.  
-    In some contexts,an application may want to handle an exception like 
-    a regular response.
-    """
+    
     def __init__(self, url, code, msg, hdrs, payload):
         self.url = url
         self.code = code
