d36e3ec83dd631864b1d3be4dd3e2a4be4a56cd2
Raymond Labbe <61295936+rlabbeptc@users.noreply.github.com>
2020-03-17 09:26:42 -0400

    Init

---
+++ kepconfig/datalogger/__init__.py
@@ -0,0 +1,14 @@
+# -------------------------------------------------------------------------
+# Copyright (c) 2020, PTC Inc. and/or all its affiliates. All rights reserved.
+# See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
+from . import log_group
+
+# #IoT Gateway Agent Types
+# MQTT_CLIENT_AGENT = 'MQTT Client'
+# REST_CLIENT_AGENT = 'REST Client'
+# REST_SERVER_AGENT = 'REST Server'
+# THINGWORX_AGENT = 'ThingWorx'
