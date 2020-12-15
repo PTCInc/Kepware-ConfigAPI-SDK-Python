7967423f08f1c766fbcab09e3fcf8fe27832f221
Raymond Labbe <61295936+rlabbeptc@users.noreply.github.com>
2020-12-15 08:53:27 -0500

    Updated Baseline tests to Pytest

--- tests/admin_test.py
+++ tests/admin_test.py
@@ -28,6 +28,10 @@ rserver_agent_name = 'REST Server'
 twx_agent_name = 'Thingworx'
 iot_item_name ="System__Date"
 
+group1 = {'common.ALLTYPES_NAME': 'Operators'}
+group2 = {'common.ALLTYPES_NAME': 'Group1'}
+group3 = {'common.ALLTYPES_NAME': 'Group2'}
+
 def HTTPErrorHandler(err):
     if err.__class__ is kepconfig.error.KepHTTPError:
         print(err.code)
@@ -38,7 +42,38 @@ def HTTPErrorHandler(err):
     else:
         print('Different Exception Received: {}'.format(err))
 
-def test_uaserver(kepware_server):
+def initialize(server):
+    pass
+
+def complete(server):
+    try: 
+        print(kepconfig.admin.user_groups.del_user_group(server,group1['common.ALLTYPES_NAME']))
+    except Exception as err:
+        HTTPErrorHandler(err)
+
+    try: 
+        print(kepconfig.admin.user_groups.del_user_group(server,group2['common.ALLTYPES_NAME']))
+    except Exception as err:
+        HTTPErrorHandler(err)
+
+    try: 
+        print(kepconfig.admin.user_groups.del_user_group(server,group3['common.ALLTYPES_NAME']))
+    except Exception as err:
+        HTTPErrorHandler(err)
+
+@pytest.fixture(scope="module")
+def server(kepware_server):
+    server = kepware_server
+    
+    # Initialize any configuration before testing in module
+    initialize(server)
+
+    # Everything below yield is run after module tests are completed
+    yield server
+    complete(server)
+    
+
+def test_uaserver(server):
     uaendpoint1 = {
         "common.ALLTYPES_NAME": "DefaultEndpoint2",
         "libadminsettings.UACONFIGMANAGER_ENDPOINT_PORT": 49331
@@ -53,106 +88,95 @@ def test_uaserver(kepware_server):
     }
 
     try: 
-        print(kepconfig.admin.ua_server.add_endpoint(kepware_server,uaendpoint1))
+        print(kepconfig.admin.ua_server.add_endpoint(server,uaendpoint1))
     except Exception as err:
         HTTPErrorHandler(err)
     
     try: 
-        print(kepconfig.admin.ua_server.del_endpoint(kepware_server,uaendpoint1['common.ALLTYPES_NAME']))
+        print(kepconfig.admin.ua_server.del_endpoint(server,uaendpoint1['common.ALLTYPES_NAME']))
     except Exception as err:
         HTTPErrorHandler(err)
     
     try: 
-        print(kepconfig.admin.ua_server.add_endpoint(kepware_server,[uaendpoint1,uaendpoint2]))
+        print(kepconfig.admin.ua_server.add_endpoint(server,[uaendpoint1,uaendpoint2]))
     except Exception as err:
         HTTPErrorHandler(err)
 
 # Endpoint 2 fails since it already exists
     try: 
-        print(kepconfig.admin.ua_server.add_endpoint(kepware_server,[uaendpoint2,uaendpoint3]))
+        print(kepconfig.admin.ua_server.add_endpoint(server,[uaendpoint2,uaendpoint3]))
     except Exception as err:
         HTTPErrorHandler(err)
 
     try: 
-        print(kepconfig.admin.ua_server.modify_endpoint(kepware_server,{"libadminsettings.UACONFIGMANAGER_ENDPOINT_ENABLE": False},uaendpoint1['common.ALLTYPES_NAME']))
+        print(kepconfig.admin.ua_server.modify_endpoint(server,{"libadminsettings.UACONFIGMANAGER_ENDPOINT_ENABLE": False},uaendpoint1['common.ALLTYPES_NAME']))
     except Exception as err:
         HTTPErrorHandler(err)
     
     # Bad Input test
     try: 
-        print(kepconfig.admin.ua_server.modify_endpoint(kepware_server,{"libadminsettings.UACONFIGMANAGER_ENDPOINT_ENABLE": False}))
+        print(kepconfig.admin.ua_server.modify_endpoint(server,{"libadminsettings.UACONFIGMANAGER_ENDPOINT_ENABLE": False}))
     except Exception as err:
         HTTPErrorHandler(err)
 
     try: 
-        print(kepconfig.admin.ua_server.modify_endpoint(kepware_server,{"common.ALLTYPES_NAME": "DefaultEndpoint3","libadminsettings.UACONFIGMANAGER_ENDPOINT_SECURITY_NONE": True}))
+        print(kepconfig.admin.ua_server.modify_endpoint(server,{"common.ALLTYPES_NAME": "DefaultEndpoint3","libadminsettings.UACONFIGMANAGER_ENDPOINT_SECURITY_NONE": True}))
     except Exception as err:
         HTTPErrorHandler(err)
     
     try: 
-        print(kepconfig.admin.ua_server.get_endpoint(kepware_server,uaendpoint1['common.ALLTYPES_NAME']))
+        print(kepconfig.admin.ua_server.get_endpoint(server,uaendpoint1['common.ALLTYPES_NAME']))
     except Exception as err:
         HTTPErrorHandler(err)
     
     try: 
-        print(kepconfig.admin.ua_server.get_all_endpoints(kepware_server))
+        print(kepconfig.admin.ua_server.get_all_endpoints(server))
     except Exception as err:
         HTTPErrorHandler(err)
     
     try: 
-        print(kepconfig.admin.ua_server.del_endpoint(kepware_server,uaendpoint1['common.ALLTYPES_NAME']))
+        print(kepconfig.admin.ua_server.del_endpoint(server,uaendpoint1['common.ALLTYPES_NAME']))
     except Exception as err:
         HTTPErrorHandler(err)
     
     try: 
-        print(kepconfig.admin.ua_server.del_endpoint(kepware_server,uaendpoint2['common.ALLTYPES_NAME']))
+        print(kepconfig.admin.ua_server.del_endpoint(server,uaendpoint2['common.ALLTYPES_NAME']))
     except Exception as err:
         HTTPErrorHandler(err)
 
-def test_user_groups(kepware_server):
+def test_user_groups(server):
     # User Group tests
-    group1 = {'common.ALLTYPES_NAME': 'Operators'}
-    group2 = {'common.ALLTYPES_NAME': 'Group1'}
-    group3 = {'common.ALLTYPES_NAME': 'Group2'}
+    assert kepconfig.admin.user_groups.add_user_group(server,group1)
     
-    
-    assert kepconfig.admin.user_groups.add_user_group(kepware_server,group1)
-    
-    assert kepconfig.admin.user_groups.del_user_group(kepware_server,group1['common.ALLTYPES_NAME'])
+    assert kepconfig.admin.user_groups.del_user_group(server,group1['common.ALLTYPES_NAME'])
 
-    assert kepconfig.admin.user_groups.add_user_group(kepware_server,[group1, group2])
+    assert kepconfig.admin.user_groups.add_user_group(server,[group1, group2])
 
 # Group 2 fails since it already exists
-    try: 
-        print(kepconfig.admin.user_groups.add_user_group(kepware_server,[group2, group3]))
-    except Exception as err:
-        HTTPErrorHandler(err)
+    assert type(kepconfig.admin.user_groups.add_user_group(server,[group2, group3])) == list
     
-    assert kepconfig.admin.user_groups.modify_user_group(kepware_server,{"libadminsettings.USERMANAGER_GROUP_ENABLED": False},
+    assert kepconfig.admin.user_groups.modify_user_group(server,{"libadminsettings.USERMANAGER_GROUP_ENABLED": False},
             group1['common.ALLTYPES_NAME'])
     
-    assert kepconfig.admin.user_groups.modify_user_group(kepware_server,{"libadminsettings.USERMANAGER_GROUP_ENABLED": True},
+    assert kepconfig.admin.user_groups.modify_user_group(server,{"libadminsettings.USERMANAGER_GROUP_ENABLED": True},
             group1['common.ALLTYPES_NAME'])
 
     # Bad Inputs
-    try: 
-        print(kepconfig.admin.user_groups.modify_user_group(kepware_server,{"libadminsettings.USERMANAGER_GROUP_ENABLED": False}))
-    except Exception as err:
-        HTTPErrorHandler(err)
+    assert kepconfig.admin.user_groups.modify_user_group(server,{"libadminsettings.USERMANAGER_GROUP_ENABLED": False}) == False
 
-    assert kepconfig.admin.user_groups.modify_user_group(kepware_server,{'common.ALLTYPES_NAME': 'Group1',"libadminsettings.USERMANAGER_GROUP_ENABLED": False})
+    assert kepconfig.admin.user_groups.modify_user_group(server,{'common.ALLTYPES_NAME': 'Group1',"libadminsettings.USERMANAGER_GROUP_ENABLED": False})
     
-    assert kepconfig.admin.user_groups.modify_user_group(kepware_server,{'common.ALLTYPES_NAME': 'Group1',"libadminsettings.USERMANAGER_GROUP_ENABLED": True})
+    assert kepconfig.admin.user_groups.modify_user_group(server,{'common.ALLTYPES_NAME': 'Group1',"libadminsettings.USERMANAGER_GROUP_ENABLED": True})
 
-    assert type(kepconfig.admin.user_groups.get_user_group(kepware_server,group1['common.ALLTYPES_NAME'])) == dict
+    assert type(kepconfig.admin.user_groups.get_user_group(server,group1['common.ALLTYPES_NAME'])) == dict
     
-    assert type(kepconfig.admin.user_groups.get_all_user_groups(kepware_server)) == list
+    assert type(kepconfig.admin.user_groups.get_all_user_groups(server)) == list
     
-    assert kepconfig.admin.user_groups.disable_user_group(kepware_server, group1['common.ALLTYPES_NAME'])
+    assert kepconfig.admin.user_groups.disable_user_group(server, group1['common.ALLTYPES_NAME'])
 
-    assert kepconfig.admin.user_groups.enable_user_group(kepware_server, group1['common.ALLTYPES_NAME'])
+    assert kepconfig.admin.user_groups.enable_user_group(server, group1['common.ALLTYPES_NAME'])
 
-def test_users(kepware_server):
+def test_users(server):
     # User tests
     user1 = {
         "common.ALLTYPES_NAME": "Client1",
@@ -176,69 +200,30 @@ def test_users(kepware_server):
         "libadminsettings.USERMANAGER_USER_PASSWORD": "Kepware400400400"      
     }
 
-    assert kepconfig.admin.users.add_user(kepware_server,user1)
+    assert kepconfig.admin.users.add_user(server,user1)
     
-    assert kepconfig.admin.users.del_user(kepware_server,user1['common.ALLTYPES_NAME'])
+    assert kepconfig.admin.users.del_user(server,user1['common.ALLTYPES_NAME'])
 
-    assert kepconfig.admin.users.add_user(kepware_server,[user1, user2])
+    assert kepconfig.admin.users.add_user(server,[user1, user2])
     
 # User 2 fails since it already exists
-    try: 
-        print(kepconfig.admin.users.add_user(kepware_server,[user2, user3]))
-    except Exception as err:
-        HTTPErrorHandler(err)
+    assert type(kepconfig.admin.users.add_user(server,[user2, user3])) == list
     
-    assert kepconfig.admin.users.modify_user(kepware_server,{"libadminsettings.USERMANAGER_USER_ENABLED": False}, user1['common.ALLTYPES_NAME'])
+    assert kepconfig.admin.users.modify_user(server,{"libadminsettings.USERMANAGER_USER_ENABLED": False}, user1['common.ALLTYPES_NAME'])
     
     # Bad Inputs
-    try: 
-        print(kepconfig.admin.users.modify_user(kepware_server,{'common.ALLTYPES_DESCRIPTION': 'TEST'}))
-    except Exception as err:
-        HTTPErrorHandler(err)
+    assert kepconfig.admin.users.modify_user(server,{'common.ALLTYPES_DESCRIPTION': 'TEST'}) == False
     
-    assert kepconfig.admin.users.modify_user(kepware_server,{"common.ALLTYPES_NAME": "Client2",'common.ALLTYPES_DESCRIPTION': 'TEST'})
+    assert kepconfig.admin.users.modify_user(server,{"common.ALLTYPES_NAME": "Client2",'common.ALLTYPES_DESCRIPTION': 'TEST'})
 
-    assert type(kepconfig.admin.users.get_user(kepware_server,user1['common.ALLTYPES_NAME'])) == dict
+    assert type(kepconfig.admin.users.get_user(server,user1['common.ALLTYPES_NAME'])) == dict
     
-    assert type(kepconfig.admin.users.get_all_users(kepware_server)) == list
+    assert type(kepconfig.admin.users.get_all_users(server)) == list
     
-    assert kepconfig.admin.users.disable_user(kepware_server, user1['common.ALLTYPES_NAME'])
+    assert kepconfig.admin.users.disable_user(server, user1['common.ALLTYPES_NAME'])
 
-    assert kepconfig.admin.users.enable_user(kepware_server, user1['common.ALLTYPES_NAME'])
+    assert kepconfig.admin.users.enable_user(server, user1['common.ALLTYPES_NAME'])
 
-    assert kepconfig.admin.users.del_user(kepware_server, user1['common.ALLTYPES_NAME'])
+    assert kepconfig.admin.users.del_user(server, user1['common.ALLTYPES_NAME'])
     
-    assert kepconfig.admin.users.del_user(kepware_server, user2['common.ALLTYPES_NAME'])
-
-# Delete User Groups
-try: 
-    print(kepconfig.admin.user_groups.del_user_group(kepware_server,group1['common.ALLTYPES_NAME']))
-except Exception as err:
-    HTTPErrorHandler(err)
-
-try: 
-    print(kepconfig.admin.user_groups.del_user_group(kepware_server,group2['common.ALLTYPES_NAME']))
-except Exception as err:
-    HTTPErrorHandler(err)
-
-try: 
-    print(kepconfig.admin.user_groups.del_user_group(kepware_server,group3['common.ALLTYPES_NAME']))
-except Exception as err:
-    HTTPErrorHandler(err)
-
-
-
-
-
-# if __name__ == "__main__":
-#     time_start = time.perf_counter()
-
-#     # This creates a server reference that is used to target all modifications of 
-#     # the Kepware configuration
-#     server = kepconfig.connection.server(host = '127.0.0.1', port = 57412, user = 'Administrator', pw = '')
-    
-#     user_manager_test(server)
-#     uaserver_test(server)
-
-#     time_end = time.perf_counter()
-#     print('Complete {}! {} - Took {} seconds'.format(os.path.basename(__file__),time.asctime(), time_end - time_start))
+    assert kepconfig.admin.users.del_user(server, user2['common.ALLTYPES_NAME'])
--- tests/server_connection_test.py
+++ tests/server_connection_test.py
@@ -25,17 +25,7 @@ rserver_agent_name = 'REST Server'
 twx_agent_name = 'Thingworx'
 iot_item_name ="System__Date"
 
-# This creates a server reference that is used to target all modifications of 
-# the Kepware configuration
-server = kepconfig.connection.server(host = 'localhost', port = 57412, user = 'Administrator', pw = '', https = False)
-tke_server = kepconfig.connection.server(host = '192.168.192.136', port = 57513, user = 'Administrator', pw = 'Kepware400400400', https = True)
-
 def test_connection_params(kepware_server):
-
-    tke_server.SSL_trust_all_certs = True
-    assert tke_server.SSL_trust_all_certs == True
-    tke_server.SSL_ignore_hostname = True
-    assert tke_server.SSL_ignore_hostname == True
 
     kepware_server.SSL_trust_all_certs = True
     assert kepware_server.SSL_trust_all_certs == True
@@ -51,7 +41,10 @@ def test_reinitialize(kepware_server):
     job = kepware_server.reinitialize()
     assert type(job) == kepconfig.connection.KepServiceResponse
 
-def test_modify_project_props(kepware_server):
+def test_project_props(kepware_server):
+    # Get Project Properties
+    assert type(kepware_server.get_project_properties()) == dict
+
     # Modify Project Properties
     project_prop = {
         "uaserverinterface.PROJECT_OPC_UA_ENABLE": False,
@@ -73,68 +66,3 @@ def test_event_log(kepware_server):
     assert type(kepware_server.get_event_log(25, None, None)) == list
     
     assert type(kepware_server.get_event_log(25, datetime.datetime.fromisoformat('2019-11-03T23:35:23.000'), datetime.datetime.now())) == list
-
-# if __name__ == "__main__":
-#     # Reinitialize TKE instance
-#     try:
-#         job = server.reinitialize()
-#         print(job)
-#     except Exception as err:
-#         # HTTPErrorHandler(err)
-#         assert False
-
-#     # Modify Project Properties
-#     project_prop = {
-#         "uaserverinterface.PROJECT_OPC_UA_ENABLE": False,
-#         "opcdaserver.PROJECT_OPC_DA_1_ENABLED": False,
-#         "opcdaserver.PROJECT_OPC_DA_2_ENABLED": False,
-#         "opcdaserver.PROJECT_OPC_DA_3_ENABLED": False
-#     }
-#     try:
-#         print(server.modify_project_properties(project_prop, force = True))
-#     except Exception as err:
-#         HTTPErrorHandler(err)
-
-#     project_prop = {
-#         "uaserverinterface.PROJECT_OPC_UA_ENABLE": True,
-#         "opcdaserver.PROJECT_OPC_DA_1_ENABLED": True,
-#         "opcdaserver.PROJECT_OPC_DA_2_ENABLED": True,
-#         "opcdaserver.PROJECT_OPC_DA_3_ENABLED": True
-#     }
-#     try:
-#         print(server.modify_project_properties(project_prop, force = True))
-#     except Exception as err:
-#         HTTPErrorHandler(err)
-
-#     #ATG when it is a device that supports it
-#     # try:
-#     #     # print(kepconfig.connectivity.device.auto_tag_gen(server, 'Channel3.Device1'))
-#     #     job = kepconfig.connectivity.device.auto_tag_gen(server, 'Channel1.Device1')
-#     #     print(job)
-#     # except Exception as err:
-#     #     HTTPErrorHandler(err)
-
-#     # Get Event Log
-#     try:
-#         print(server.get_event_log(25, None, None))
-#     except Exception as err:
-#         HTTPErrorHandler(err)
-#     try:
-#         print(server.get_event_log(25, datetime.datetime.fromisoformat('2019-11-03T23:35:23.000'), datetime.datetime.now()))
-#     except Exception as err:
-#         HTTPErrorHandler(err)
-
-
-#     # # Execute Module Tests
-#     # admin.user_manager_test(server)
-   
-#     # # runs on TKE since UA Endpoint management isn't available in TKS
-#     # admin.uaserver_test(tke_server)
-    
-#     # connectivity.connectivity_test(server)
-#     # iot_gateway.iot_gateway_test(server)
-#     # datalogger.datalogger_test(server)
-
-
-#     time_end = time.perf_counter()
-#     print('Complete {}! {} - Took {} seconds'.format(os.path.basename(__file__),time.asctime(), time_end - time_start))
