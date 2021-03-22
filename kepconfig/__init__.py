9245e561e8043cafcb7f844a38f908b3e46c8045
Raymond Labbe <61295936+rlabbeptc@users.noreply.github.com>
2021-03-22 14:39:52 -0400

    1.0.0 Release

--- kepconfig/__init__.py
+++ kepconfig/__init__.py
@@ -1,10 +1,10 @@
 # -------------------------------------------------------------------------
-# Copyright (c) 2020, PTC Inc. and/or all its affiliates. All rights reserved.
+# Copyright (c) PTC Inc. and/or all its affiliates. All rights reserved.
 # See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
 
-
+__version__ = "1.0.0"
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 from . import connection, error
 
