b9d014221a6299f01815376ec111d6ae866d08de
Ray Labbe <61295936+rlabbeptc@users.noreply.github.com>
2023-07-11 17:46:32 -0400

    Init Commit for UAG functionality

---
+++ kepconfig/ua_gateway/__init__.py
@@ -0,0 +1,11 @@
+# -------------------------------------------------------------------------
+# Copyright (c) 2023 PTC Inc. and/or all its affiliates. All rights reserved.
+# See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+r"""`ua_gateway` module provides support for Kepware's UA Gateway plug-in 
+specific objects within the Kepware Configuration API
+"""
+
+from . import certificates, client, server
